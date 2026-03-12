59d3e9e3f07fb9b9d6cd233b539f87298f52402e
PracticeGit <aishwaryaan700@gmail.com>
2026-03-12 14:24:49 +0530

    Updated Propeller logo and link

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Propeller DirtMate
         </a>
       </header>
     </div>
